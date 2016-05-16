530e74c9fbfc9c3fbef269f7e9f60aeacb1d58f7
CoxRos <cosimo@preventivi.it>
2016-05-16 12:52:40 +0200

    instanziazione progetto

---
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/HomePage.java
@@ -0,0 +1,13 @@
+package it.uniba.di.cosimo.sms16.mydib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
