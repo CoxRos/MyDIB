988063be434ae35af2e2230763b1650e56c0da1c
CoxRos <cosimo@preventivi.it>
2016-05-17 15:00:19 +0200

    init uni_contacts

---
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/flusso/university_contacts/Contacts.java
@@ -0,0 +1,15 @@
+package it.uniba.di.cosimo.sms16.mydib.flusso.university_contacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.uniba.di.cosimo.sms16.mydib.R;
+
+public class Contacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
