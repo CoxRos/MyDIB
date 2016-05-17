268c0fa6883f77a1dc202e47724ca99b3ca4ca31
Sergio Corvino <sergio.corvino@preventivi.it>
2016-05-17 12:59:50 +0200

    aggiunta toolbar homepage

--- app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
@@ -2,16 +2,39 @@ package it.uniba.di.cosimo.sms16.mydib.homepage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import it.uniba.di.cosimo.sms16.mydib.R;
 
 public class HomePage extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 
-        //modifica
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_info) {
+            Log.d("HOME", "hai cliccato le info");
+        } else if(id == R.id.action_login) {
+            Log.d("HOME", "vai alla login con popup");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
