300fb77e7213d58ef00b166c2b836e8aa1b98f73
Sergio Corvino <sergio.corvino@preventivi.it>
2016-05-18 17:16:13 +0200

    aggiunti button, css e collegati i contatti

--- app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
@@ -7,29 +7,66 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import it.uniba.di.cosimo.sms16.mydib.R;
 import it.uniba.di.cosimo.sms16.mydib.entity.uni_contacts.E_Contacts;
+import it.uniba.di.cosimo.sms16.mydib.flusso.university_contacts.Contacts;
 import it.uniba.di.cosimo.sms16.mydib.system.GestioneSessione;
 
 public class HomePage extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private Button btnInfo, btnNews, btnContatti, btnSvago;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("HomePage");
         setSupportActionBar(toolbar);
+
+        //catturo gli id dei button
+        btnInfo = (Button)findViewById(R.id.btnInfo);
+        btnNews = (Button)findViewById(R.id.btnNews);
+        btnContatti = (Button)findViewById(R.id.btnContatti);
+        btnSvago = (Button)findViewById(R.id.btnSvago);
 
         // Da togliere
         GestioneSessione.contacts.add(new E_Contacts("Ministro", "E' la descrizione del ministro", "ministro@gmail.com", "0805392468"));
         GestioneSessione.contacts.add(new E_Contacts("Deputato","E' la descrizione del deputato","deputato@gmail.com","0805392468"));
         //------------
-        //Da spostare nel bottone VAI A CONTATTI
-        Intent intent = new Intent(this, it.uniba.di.cosimo.sms16.mydib.flusso.university_contacts.Contacts.class);
-        startActivity(intent);
-        //---------
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnContatti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), Contacts.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSvago.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
