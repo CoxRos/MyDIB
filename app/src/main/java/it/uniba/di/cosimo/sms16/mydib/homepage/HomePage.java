5e19ef59b139385ec16ca702381daf86f80776ed
CoxRos <cosimo@preventivi.it>
2016-05-19 14:56:25 +0200

    base ricerca_utente

--- app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
@@ -10,6 +10,9 @@ import android.view.MenuItem;
 
 import it.uniba.di.cosimo.sms16.mydib.R;
 import it.uniba.di.cosimo.sms16.mydib.entity.uni_contacts.E_Contacts;
+import it.uniba.di.cosimo.sms16.mydib.entity.uni_search.UserSearched;
+import it.uniba.di.cosimo.sms16.mydib.flusso.info_university.Info;
+import it.uniba.di.cosimo.sms16.mydib.flusso.ricerca_utenti.RicercaUtenti;
 import it.uniba.di.cosimo.sms16.mydib.flusso.ristoro.Ristoro_Maps;
 import it.uniba.di.cosimo.sms16.mydib.system.GestioneSessione;
 
@@ -25,7 +28,23 @@ public class HomePage extends AppCompatActivity {
 
         // Da togliere
         GestioneSessione.contacts.add(new E_Contacts("Ministro", "E' la descrizione del ministro", "ministro@gmail.com", "0805392468"));
-        GestioneSessione.contacts.add(new E_Contacts("Deputato","E' la descrizione del deputato","deputato@gmail.com","0805392468"));
+        GestioneSessione.contacts.add(new E_Contacts("Deputato", "E' la descrizione del deputato", "deputato@gmail.com", "0805392468"));
+
+        GestioneSessione.userSearched.add(new UserSearched("1", "Tizio", "Caio", "Docente", "tizio@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("2","Carlo","Pizzo","Studente","pizzo@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("3", "Fluvio", "Travia", "Dirigente", "travia@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("4","Parolo","Creta","Docente","creta@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("5","Terry","henry","Docente","henry@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("6","Tizio","Caio","Docente","tizio@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("7","Carlo","Pizzo","Studente","pizzo@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("8","Fluvio","Travia","Dirigente","travia@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("9","Parolo","Creta","Docente","creta@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("10","Terry","henry","Docente","henry@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("11","Tizio","Caio","Docente","tizio@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("12","Carlo","Pizzo","Studente","pizzo@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("13","Fluvio","Travia","Dirigente","travia@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("14","Parolo","Creta","Docente","creta@gmail.com"));
+        GestioneSessione.userSearched.add(new UserSearched("15","Terry","henry","Docente","henry@gmail.com"));
         //------------
         //Da spostare nel bottone VAI A CONTATTI
         //Intent intent = new Intent(this, it.uniba.di.cosimo.sms16.mydib.flusso.university_contacts.Contacts.class);
@@ -36,7 +55,7 @@ public class HomePage extends AppCompatActivity {
         //startActivity(intent);
         //---------
         //Da spostare nel bottone RISTORO
-        Intent intent = new Intent(this, Ristoro_Maps.class);
+        Intent intent = new Intent(this, RicercaUtenti.class);
         startActivity(intent);
 
     }
--- app/src/main/java/it/uniba/di/cosimo/sms16/mydib/system/GestioneSessione.java
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/system/GestioneSessione.java
@@ -3,19 +3,28 @@ package it.uniba.di.cosimo.sms16.mydib.system;
 import java.util.ArrayList;
 
 import it.uniba.di.cosimo.sms16.mydib.entity.uni_contacts.E_Contacts;
+import it.uniba.di.cosimo.sms16.mydib.entity.uni_search.UserSearched;
 
 /**
  * Created by Cosimo on 16/05/2016.
  */
 public class GestioneSessione {
 
+    public static ArrayList<E_Contacts> contacts = new ArrayList<E_Contacts>();
+    public static ArrayList<UserSearched> userSearched = new ArrayList<UserSearched>();
+
     public static void setContacts(ArrayList<E_Contacts> contacts) {
         GestioneSessione.contacts = contacts;
     }
-
-    public static ArrayList<E_Contacts> contacts = new ArrayList<E_Contacts>();
-
     public static ArrayList<E_Contacts> getContacts() {
         return contacts;
+    }
+
+    public static ArrayList<UserSearched> getUserSearched() {
+        return userSearched;
+    }
+
+    public static void setUserSearched(ArrayList<UserSearched> userSearched) {
+        GestioneSessione.userSearched = userSearched;
     }
 }
