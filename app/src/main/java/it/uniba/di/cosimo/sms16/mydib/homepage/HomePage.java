32808590adab3522d2e1119be4a828a457512e07
CoxRos <cosimo@preventivi.it>
2016-05-19 16:01:05 +0200

    checkpoint homepage,flusso

--- app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/homepage/HomePage.java
@@ -15,7 +15,7 @@ import it.uniba.di.cosimo.sms16.mydib.entity.uni_search.UserSearched;
 import it.uniba.di.cosimo.sms16.mydib.system.GestioneSessione;
 
 /*
-    Master delle 15.48
+    Master delle 16.00
  */
 public class HomePage extends OptionBarActivity {
 
