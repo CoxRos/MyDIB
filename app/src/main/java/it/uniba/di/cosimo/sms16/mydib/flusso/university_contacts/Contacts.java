977e814881f82655f8ec0740fc18c6de930c9f3d
CoxRos <cosimo@preventivi.it>
2016-05-24 13:29:30 +0200

    network contatti

--- app/src/main/java/it/uniba/di/cosimo/sms16/mydib/flusso/university_contacts/Contacts.java
+++ app/src/main/java/it/uniba/di/cosimo/sms16/mydib/flusso/university_contacts/Contacts.java
@@ -1,6 +1,7 @@
 package it.uniba.di.cosimo.sms16.mydib.flusso.university_contacts;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,19 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import it.uniba.di.cosimo.sms16.mydib.R;
 import it.uniba.di.cosimo.sms16.mydib.entity.uni_contacts.E_Contacts;
+import it.uniba.di.cosimo.sms16.mydib.network.Network;
 import it.uniba.di.cosimo.sms16.mydib.system.GestioneSessione;
 
 public class Contacts extends Activity {
@@ -17,10 +29,17 @@ public class Contacts extends Activity {
     ListView contacts;
     TextView email,phone;
     Intent emailIntent;
+
+    RequestQueue queue;
+    ContacsAdapter listAdapter;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uni_contacts);
+
+        queue = Network.getInstance(getApplicationContext()).
+                getRequestQueue();
 
         emailIntent = new Intent(android.content.Intent.ACTION_SEND);
         emailIntent.setType("plain/text");
@@ -30,12 +49,9 @@ public class Contacts extends Activity {
         phone = (TextView) findViewById(R.id.textPhone);
 
         contacts = (ListView) findViewById(R.id.listUniContacts);
-        ContacsAdapter listAdapter = new ContacsAdapter(getApplicationContext(),R.layout.list_contact_detail);
+        listAdapter = new ContacsAdapter(getApplicationContext(),R.layout.list_contact_detail);
         contacts.setAdapter(listAdapter);
-
-        for(E_Contacts value : GestioneSessione.getContacts()) {
-            listAdapter.add(value);
-        }
+        setUI("https://api.myjson.com/bins/4h2z8");
 
         //Con il click sulla lista sto andando a inviare l'email corrispondente
         contacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -50,5 +66,43 @@ public class Contacts extends Activity {
             }
         });
 
+    }
+
+    private void setUI(String url) {
+
+        JsonArrayRequest jsObjRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for(int i= 0; i< response.length(); i++) {
+                            try {
+                                JSONObject oggettoNota = response.getJSONObject(i);
+                                String titolo = oggettoNota.getString("titolo");
+                                String descrizione = oggettoNota.getString("descrizione");
+                                String email = oggettoNota.getString("email");
+                                String telefono = oggettoNota.getString("phone");
+                                listAdapter.add(new E_Contacts(titolo,descrizione,email,telefono));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        progressDialog.dismiss();
+                    }
+
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("JSON Errore connessione contatti: " + error.networkResponse);
+                        progressDialog.dismiss();
+
+                    }
+                });
+        Network.getInstance(getApplicationContext()).addToRequestQueue(jsObjRequest);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Attendere");
+        progressDialog.setMessage("Caricamento messaggi");
+        progressDialog.show();
     }
 }
